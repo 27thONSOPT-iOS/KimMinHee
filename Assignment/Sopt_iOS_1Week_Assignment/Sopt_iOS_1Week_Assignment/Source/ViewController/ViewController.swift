0c2c94fe450b59baf880c72dc07dbb49d4e38af4
xwoud <xwoud@naver.com>
2020-10-12 18:52:38 +0900

    [UPDATE] 1차 과제 프로젝트 생성

---
+++ Assignment/Sopt_iOS_1Week_Assignment/Sopt_iOS_1Week_Assignment/Source/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt_iOS_1Week_Assignment
+//
+//  Created by 김민희 on 2020/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

65ab42bd06415cd20fded2c419a846b3c83120bf
xwoud <xwoud@naver.com>
2020-10-24 23:45:26 +0900

    [UPDATE] 2주차 세미나 폴더 생성 후 assets 추가

---
+++ Assignment/Sopt_iOS_2Week_Assignment/Sopt_iOS_2Week_Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sopt_iOS_2Week_Assignment
+//
+//  Created by 김민희 on 2020/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

871cae0204cefdba04c0345177ca82b20894f093
xwoud <xwoud@naver.com>
2020-11-13 15:22:53 +0900

    [UPDATE] 4주차 과제 - tab bar 연결 성공

---
+++ Assignment/Sopt_iOS_Assignment/Sopt_iOS_Assignment/Source/ViewController/LoginTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginTabBarController.swift
+//  Sopt_iOS_Assignment
+//
+//  Created by 김민희 on 2020/11/13.
+//
+
+import UIKit
+
+class LoginTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBar()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+extension LoginTabBarController {
+    func setTabBar() {
+        
+        self.tabBar.tintColor = UIColor(red: 254/255, green: 214/255, blue: 0/255, alpha: 1)
+        
+        guard let loginVC = self.storyboard?.instantiateViewController(identifier: "TabLoginVC") as? TabLoginVC else {
+            return
+        }
+        let collectionStoryboard = UIStoryboard.init(name: "Collection", bundle: nil)
+        
+        guard let collectionVC = collectionStoryboard.instantiateViewController(identifier: "CollectionTestVC") as? CollectionTestVC else {
+            return
+        }
+        loginVC.tabBarItem.title = "Home"
+        loginVC.tabBarItem.image = UIImage(systemName: "house")
+        
+        collectionVC.tabBarItem.title = "SOPT+WORKING"
+        collectionVC.tabBarItem.image = UIImage(systemName: "paperplane")
+//        collectionVC.tabBarItem.selectedImage = UIImage(systemName: "person.fill")
+        
+        setViewControllers([loginVC, collectionVC], animated: true) }
+}
---
+++ Assignment/Sopt_iOS_Assignment/Sopt_iOS_Assignment/Source/ViewController/TabLoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  TabLoginVC.swift
+//  Sopt_iOS_Assignment
+//
+//  Created by 김민희 on 2020/11/13.
+//
+
+import UIKit
+
+class TabLoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
